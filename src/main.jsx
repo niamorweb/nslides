53afe0f00fae7bf46e929c7f297352049cf14ae4
NiamorDev <niamorpro@proton.me>
2023-02-25 22:30:39 +0100

    update path

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
@@ -8,11 +8,13 @@ import Codes from "./pages/Codes";
 import "./style/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/codes" element={<Codes />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="nslides/" element={<App />} />
+        <Route path="nslides/codes" element={<Codes />} />
+        <Route path="nslides/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
