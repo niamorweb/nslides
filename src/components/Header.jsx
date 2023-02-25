af9097af5f1a2c0fbdf595e1311af594edb67f15
NiamorDev <niamorpro@proton.me>
2023-02-25 22:40:57 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,17 @@ export default function Header({ bgColor }) {
         <span className="text-2xl  sm:text-3xl font-semibold ">Ntricks</span>
         <ul className="flex items-center text-sm gap-3 sm:gap-8 sm:text-lg font-medium ">
           <li>
-            <NavLink exact to="nslides/" activeclassname="active">
+            <NavLink exact to="/" activeclassname="active">
               Home
             </NavLink>
           </li>
           <li>
-            <NavLink exact to="nslides/codes" activeclassname="active">
+            <NavLink exact to="/codes" activeclassname="active">
               Codes
             </NavLink>
           </li>
           <li>
-            <NavLink exact to="nslides/about" activeclassname="active">
+            <NavLink exact to="/about" activeclassname="active">
               About
             </NavLink>
           </li>
